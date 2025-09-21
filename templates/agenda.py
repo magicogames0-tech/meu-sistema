058d177fbc97f75b1787ac0cf596854df52c2a01
magicogames0-tech <magicogames0@gmail.com>
2025-09-20 23:07:08 -0300

    agenda.py

---
+++ templates/agenda.py
@@ -0,0 +1,6 @@
+import os
+import sqlite3
+
+# Caminho absoluto para o banco na pasta do projeto
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "agenda.db")
